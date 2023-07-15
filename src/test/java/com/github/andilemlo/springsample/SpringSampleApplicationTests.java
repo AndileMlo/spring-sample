a9a4986759f77469b378f6d4d5851de589d2708d
AndileMlo <138500253+AndileMlo@users.noreply.github.com>
2023-07-15 12:26:48 +0200

    added spring initializr code

---
+++ src/test/java/com/github/andilemlo/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andilemlo.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
